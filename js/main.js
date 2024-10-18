e66a1d618a88fc6b1c7d2e943b12929c478b101a
harshita9340 <harshitapattaiya@gmail.com>
2024-10-19 00:56:58 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let currentPlayer="X";
+let array=Array(9).fill(null);
+var gameResult = document.getElementById('game-result');
+var turn = document.getElementById('currentTurn');
+function checkWinner(){
+    if((array[0]!=null && array[0]==array[1]&&array[1]==array[2])||
+    (array[3]!==null && array[3]==array[4]&&array[4]==array[5])||
+    (array[6]!==null && array[6]==array[7]&&array[7]==array[8])||
+    (array[0]!==null && array[0]==array[3]&&array[3]==array[6])||
+    (array[1]!==null && array[1]==array[4]&&array[4]==array[7])||
+    (array[2]!==null && array[2]==array[5]&&array[5]==array[8])||
+    (array[0]!==null && array[0]==array[4]&&array[4]==array[8])||
+    (array[2]!==null && array[2]==array[4]&&array[4]==array[6]))
+    {
+        gameResult.innerHTML = `Winner is ${currentPlayer}`;
+        return;
+    }
+   if(!array.some(e=>e===null)){
+    gameResult.innerHTML =`<p class="text-danger">Draw!!</p>`;
+    return;
+   }
+
+
+}
+
+function handleClick(el){
+    const id=Number(el.id);
+    if(array[id]!==null)return;
+    array[id]=currentPlayer;
+    el.innerText=currentPlayer;
+    checkWinner();
+    currentPlayer=currentPlayer==="X"?"0":"X";
+    turn.innerHTML = currentPlayer;
+}
